13a126d16c2b06535a85d68bede5d476f2dc1ad1
Afraz396 <afraz@psts-sg.com>
2026-09-23 12:34:52 +0530

    cors error fixed

--- main.py
+++ main.py
@@ -31,9 +31,10 @@ FRONTEND_URL = os.getenv(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        FRONTEND_URL,
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
         "http://localhost:5173",
-        "http://localhost:3000",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
